f6fba8ff7a49e82c46e21f96011d619a00564806
Yusuf YALIM <yusufyalim@gmail.com>
2014-11-19 00:28:58 +0200

    refactor lib/tureng.rb

--- lib/tureng.rb
+++ lib/tureng.rb
@@ -1,109 +1,89 @@
 # coding: utf-8
-#require 'tureng/version.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
 
 # Mustafa Serhat DÜNDAR
 # msdundars@gmail.com
-
+# require 'tureng/version.rb'
 require 'nokogiri'
 require 'open-uri'
 require 'text-table'
 
-BASE_URL = 'http://tureng.com'
-SEARCH_URL = BASE_URL + '/search/'
+module Tureng
+  BASE_URL = 'http://tureng.com/'
+  SEARCH_URL = BASE_URL + 'search/' 
 
-class Tureng
-  def initialize(word)
-    @word = word
-  end
+  class Translator
+    attr_reader :word, :limit, :language_code, :table_type, :table, :status,
+      :table_tag, :response
 
-  def get_response
-    uri = SEARCH_URL + @word
-    @response = Nokogiri::HTML(open(uri), nil, 'utf-8')
-  end
-
-  def parse_tables_from_response
-    @en_tr_table = @response.css("table[id='englishResultsTable']")
-    @en_tr_table_full = @response.css("table[id='englishFullResultsTable']")
-  end
-
-  def draw_results(params)
-    parse_tables_from_response
-
-    params == "en_to_tr" ? (source = @en_tr_table) : (source = @en_tr_table_full)
-
-    # draw an empty table
-    table = Text::Table.new(
-    	horizontal_padding: 1,
-	    vertical_boundary: '=',
-	    horizontal_boundary: '|',
-      boundary_intersection: 'O'
-    )
-
-    # parse tr tags
-    tr_tags = source.css('tr')
-
-    # remove heading from tr_tags and save it to another variable
-    headings = tr_tags.shift.css('th').map(&:text)
-
-
-    # add headings to table
-    table.head = headings.values_at(1, 3, 4).unshift('##')
-
-    tr_tags.each do |row|
-      table.rows << row.css('td').map(&:text).values_at(0, 1, 3, 4)
+    def initialize(word, settings = {})
+      @word = word
+      @limit = settings[:limit]
+      @language_code = settings[:language_code] || :en
+      @table_type = settings[:table_type] || ''
+      @table = Text::Table.new(
+        horizontal_padding: 1,
+        vertical_boundary: '=',
+        horizontal_boundary: '|',
+        boundary_intersection: 'O'
+      )
+      @table_id = set_table_id
+      @response = get_response
+      @status = @response.css('h1')[1].text.strip
+      @table_tag = parse_table_from_response
     end
 
-    # draw table
-    puts table.to_s
-  end
-
-  def get_suggestions
-    # draws suggestions table
-    table = Text::Table.new
-    table.head = [' Öneriler ']
-
-    table.rows = @response.css('li a').map(&:text)
-
-    # draw table
-    puts table.to_s
-  end
-
-  def draw_all_results
-    get_response
-    status = @response.css('h1')[1].text.strip
-
-    if status == "Did you mean that?"
-      puts "Aradığınız kelime bulunamadı. Bunlardan birini yazmak istemiş olabilir misiniz?"
-      get_suggestions
-    elsif status == "Term not found"
-      puts "Aradığınız kelime bulunamadı."
-    else
-      draw_results("en_to_tr")
-      draw_results("en_to_tr_full")
+    def set_table_id
+      language = { en: 'english', tr: 'turkish' }
+      "#{language[language_code]}#{table_type.capitalize}ResultsTable"
     end
-  end
-end
 
-# Dead simple, quite basic interpretation
-def translate
-  quit_values = ["q", "quit", "exit"]
-  while true
-    puts "Tureng'de aramak istediğiniz kelimeyi girin: "
-    #begin
-      input = gets.chomp
-      unless quit_values.include?(input)
-        c = Tureng.new(input)
-        c.draw_all_results
-      else
-        break
+    def get_response
+      Nokogiri::HTML(open(SEARCH_URL + word), nil, 'utf-8')
+    end
+
+    def parse_table_from_response
+      response.css("table[id='#{@table_id}']")
+    end
+
+    def fill_table
+      # parse tr tags
+      tr_tags = table_tag.css('tr')
+
+      # remove heading from tr_tags and save it to another variable
+      headings = tr_tags.shift.css('th').map(&:text)
+
+      # add headings to table
+      table.head = headings.values_at(1, 3, 4).unshift('##')
+
+      # add translation to table
+      tr_tags.each_with_index do |row, index|
+        table.rows << row.css('td').map(&:text).values_at(0, 1, 3, 4)
+        # end each loop if we reach or above limit
+        # break if limit is set and index greater than or equal to limit
+        break if limit && index >= (limit - 1)
       end
-    #rescue 
-      puts "Hatalı giriş, tekrar deneyin!"
-    #end
+    end
+
+    def get_suggestions
+      table.head = ['Öneriler']
+      table.rows = @response.css('li a').map(&:text)
+    end
+
+    def draw_table
+      if status == "Did you mean that?"
+        puts "Aradığınız kelime bulunamadı. Bunlardan birini yazmak istemiş olabilir misiniz?"
+        get_suggestions
+      elsif status == "Term not found"
+        puts "Aradığınız kelime bulunamadı."
+        return nil
+      else
+        fill_table
+      end
+
+      puts table.to_s
+    end
   end
 end
-
-translate
